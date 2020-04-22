66e18678a648f825db5b32ea128dccd8adfcef73
mathan.raj <mathan.raj@seco.com>
2020-04-22 11:57:53 +0530

    imx-sc-seco firmware

---
+++ mx8qx/platform/main/build_info.h
@@ -0,0 +1,11 @@
+#ifndef BUILD_INFO_H
+#define BUILD_INFO_H
+
+#define SCFW_BRANCH imx_4.14.98_2.0.0_ga
+#define SCFW_BUILD 3358UL
+#define SCFW_COMMIT 0x4e91f37bUL
+#define SCFW_DATE "Oct 04 2019"
+#define SCFW_DATE2 Oct_04_2019
+#define SCFW_TIME "02:38:08"
+
+#endif
